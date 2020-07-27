37074cffc2271aa36e37b26642e8f7300eebcc5a
unknown <aleksandlario@gmail.com>
2020-07-27 18:56:16 +0100

    Add getAttr func

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,44 +1,55 @@
-'use strict'
+"use strict";
 
-var _react = require('react')
+var _react = require("react");
 
-var _react2 = _interopRequireDefault(_react)
+var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj }
+  return obj && obj.__esModule ? obj : { default: obj };
 }
 
 var format = function format(string) {
   return string
-    .split(' ')
-    .map(function(s) {
-      return s.replace(/^\w/, function(s) {
-        return s.toUpperCase()
-      })
+    .split(" ")
+    .map(function (s) {
+      return s.replace(/^\w/, function (s) {
+        return s.toUpperCase();
+      });
     })
-    .join(' ')
-}
+    .join(" ");
+};
 
 var getFonts = function getFonts(options) {
-  return options.fonts
-    .map(format)
-    .join('|')
-    .replace(/ /g, '+')
-}
+  return options.fonts.map(format).join("|").replace(/ /g, "+");
+};
 
 function getDisplay(options) {
-  return options.display ? '&display=' + options.display : ''
+  return options.display ? "&display=" + options.display : "";
 }
 
-exports.onRenderBody = function(_ref, options) {
-  var setHeadComponents = _ref.setHeadComponents
+exports.onRenderBody = function (_ref, options) {
+  var setHeadComponents = _ref.setHeadComponents;
 
-  var link = 'https://fonts.googleapis.com/css?family=' + getFonts(options) + getDisplay(options)
-  setHeadComponents([
-    _react2.default.createElement('link', {
-      key: 'fonts',
-      href: link,
-      rel: 'stylesheet'
-    })
-  ])
-}
+  var link =
+    "https://fonts.googleapis.com/css?family=" +
+    getFonts(options) +
+    getDisplay(options);
+
+  function getAttributes() {
+    return [
+      {
+        key: "fonts",
+        href: link,
+        rel: "stylesheet",
+      },
+      option.attributes || {},
+    ].reduce(function (r, o) {
+      Object.keys(o).forEach(function (k) {
+        r[k] = o[k];
+      });
+      return r;
+    }, {});
+  }
+
+  setHeadComponents([_react2.default.createElement("link", getAttributes())]);
+};
